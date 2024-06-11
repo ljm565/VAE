cde66f79b9ef5a796f09f48e3c7eb0f6258e93f3
Jun-Min Lee <ljm56897@gmail.com>
2024-06-12 08:08:03 +0900

    feat: utils logging name 수정

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -10,7 +10,7 @@ from tqdm import tqdm as tqdm_original
 import torch
 
 
-LOGGING_NAME = 'Autoencoder_2.0'
+LOGGING_NAME = 'VAE_2.0'
 VERBOSE = True
 RANK = int(os.getenv('RANK', -1))
 MACOS, LINUX, WINDOWS = (platform.system() == x for x in ['Darwin', 'Linux', 'Windows'])  # environment booleans
